c0394a3b254d76ed0d2c493ad1a1a83a6969298b
Shohjaxon <vghfh0508@bk.ru>
2022-12-25 11:13:02 +0500

    first commit

---
+++ src/Login.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import Register from './Register';
+import './Login.css';
+function Login() {
+  function loginFunc(){
+
+    var username = document.getElementById('username').value;
+    var pass = document.getElementById('password').value;
+
+var user = localStorage.getItem(username);
+var data = JSON.parse(user);
+console.log(data);
+
+if(user == null){
+  console.log('wrong username');
+}
+else if(username==data.username && pass==data.password){
+console.log('logged in');
+}
+else{
+  console.log('wrong password');
+}
+  }
+  return (
+<>
+<div className='box'>
+<div className='form'>
+    <h2>Sign in</h2>
+    <div className='inputBox'>
+<input id='username' type='text' required='required'/>
+<span>Username</span>
+<i></i>
+    </div>
+    <div className='inputBox'>
+<input id='password' type='password' required='required'/>
+<span>Password</span>
+<i></i>
+    </div>
+    <div className='links'>
+        <a href='https://support.google.com/accounts/answer/41078?hl=en&co=GENIE.Platform%3DAndroid'>Forgot password</a>
+        <a href='Register.js'>Create account</a>
+    </div>
+    <input onClick={loginFunc} type="Submit" value='Login'/>
+</div>
+</div>
+</>
+  )
+}
+
+export default Login
---
+++ src/Register.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import './Register.css';
+import swal from 'sweetalert';
+
+function Register() {
+
+    function signup(e){
+
+        swal("Good job!", "You create account", "success");
+
+        var username = document.getElementById('username').value;
+        var pass = document.getElementById('password').value;
+    
+        var user ={
+            username: username,
+            password:pass,
+        };
+        
+        var json = JSON.stringify(user);
+        localStorage.setItem(username,json);
+        console.log('user added');
+        }
+  return (
+    <>
+    <div className='box'>
+<div className='form'>
+    <h2>Sign up</h2>
+    <div className='inputBox'>
+<input id='username' type='text' required='required'/>
+<span>Username</span>
+<i></i>
+    </div>
+    <div className='inputBox'>
+<input id='password' type='password' required='required'/>
+<span>Password</span>
+<i></i>
+    </div>
+    <div className='links'>
+        <a href='Login.js'>Sign in</a>
+    </div>
+    <input onClick={signup} type="Submit" value='Create'/>
+</div>
+</div>
+    </>
+  )
+}
+
+export default Register
